90f4884bd4a9a732dd59a7ee3c282d352adbff13
Álvaro Prudêncio <43001689+im-a-Cloud@users.noreply.github.com>
2022-10-17 18:40:41 -0300

    Q5, funciona e foi aceita no codeforces

--- Atividade dupla #1/q5.cpp
+++ Atividade dupla #1/q5.cpp
@@ -9,33 +9,36 @@ int main() {
   string string_concatenada;
   cin >> tamanho;
   string string_final;
-  int contagem = 0;
+  int contagemAtual = 0;
+  int contagemTotal = 0;
   for (int i = 0; i < tamanho; i++) {
     cin >> letra;
     string_concatenada += letra;
   }
   for (int i = 0; i < string_concatenada.size(); i++)
   {
-    if (string_concatenada[i] == 'a' and string_concatenada[i + 1] == 'a') {
-      contagem++;
+    if (string_concatenada[i] == 'a') 
+    {
+      contagemAtual++;
+      if( (i == tamanho -1))
+      {
+        if (contagemAtual > 1)
+        {
+          contagemTotal = contagemTotal + contagemAtual;
+        }
+        
+      }
+    }
+    else if(contagemAtual > 1)
+    {
+      contagemTotal = contagemTotal + contagemAtual;
+      contagemAtual = 0;
+    }
+    else
+    {
+      contagemAtual = 0;
     }
   }
-  if (contagem == 0)
-  {
-    cout << contagem << endl;
-    return 0;
-  }
-  contagem = 1;
-  regex bab("bab");
-  regex aba("aba");
-  string_final = regex_replace(string_concatenada, bab, "");
-  string_final = regex_replace(string_final, aba, "");
-
-  for (int i = 0; i < string_final.size(); i++) {
-    if (string_final[i] == 'a' and string_final[i + 1] == 'a') {
-      contagem++;
-    }
-  }
-  cout << contagem << endl;
+  cout << contagemTotal << endl;
   return 0;
 }
